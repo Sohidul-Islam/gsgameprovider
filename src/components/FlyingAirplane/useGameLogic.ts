adbf8b8addeb0f17e1476bef04e3bf0ac4856bf8
Sohidul Islam <63615353+Sohidul-Islam@users.noreply.github.com>
2025-09-01 16:51:53 +0600

    Update useGameLogic.ts

--- src/components/FlyingAirplane/useGameLogic.ts
+++ src/components/FlyingAirplane/useGameLogic.ts
@@ -232,7 +232,12 @@ export function useGameLogic() {
     const currentMultiplier = Math.pow(Math.E, clampedElapsed / 4); // Slower growth (divided by 4 instead of 2)
 
     const probabilityOfCrash = Math.random();
-    if (probabilityOfCrash < 0.01 && currentMultiplier >= 10 && currentMultiplier <= 300) {
+    // if (probabilityOfCrash < 0.01 && currentMultiplier >= 10 && currentMultiplier <= 300) {
+    //   setIsCrashed(true);
+    //   }
+
+
+      if (probabilityOfCrash < 0.01) {
       setIsCrashed(true);
       }
 
