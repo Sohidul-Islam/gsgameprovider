35cd055d94f1b11814160568f238e7ec4af68e69
Sohidul Islam <63615353+Sohidul-Islam@users.noreply.github.com>
2025-10-19 15:12:45 +0600

    Update useGameLogic.ts

--- src/components/FlyingAirplane/useGameLogic.ts
+++ src/components/FlyingAirplane/useGameLogic.ts
@@ -4,7 +4,7 @@ import type { GameState, PlanePosition } from "./types";
 
 // API configuration
 // const BASE_URL = "http://localhost:3000";
-const BASE_URL = "https://glorypos.com/gs-server";
+const BASE_URL = "https://api.gamestar365.com";
 const api = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
